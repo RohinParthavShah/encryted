6843e6a388a523f33de01e2fbbcb46dcd7661e83
RohinParthavShah <83176943+RohinParthavShah@users.noreply.github.com>
2022-02-21 12:09:46 -0600

    Add files via upload

---
+++ untitled8.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 08:40:02 2022
+
+@author: Shree Ram
+"""
+name="rohin"
+
+for letter in name :
+    
+    print(letter)
+    
+    print(ord(letter))
